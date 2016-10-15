0ff4b2545ad478bd8e961bc9b0c6c7daba683de9
savierx <kibett4@live.com>
2016-10-15 16:10:20 +0300

    Revert "Revert "bloggerino, firstcommit""
    This reverts commit ce73d88a389ebb8b14a5e5a88976a12ff7c72a53.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
 from django.contrib.gis.feeds import Feed
-from blog.views import archive
-from blog.models import RSSFeed
+#from blog.views import archive
+from blog.models import RSSFeed, Art, BlogPost, Category
+from .views import *
 
+app_name = 'blog'
 urlpatterns = [
-    url(r'^$', archive),
+    url(r'^$', ArchiveView),
     url(r'^feeds/(?P<url>.*)/$', Feed, {'feed_dict': {'rss': RSSFeed}}),
+    url(r'^(?P<slug>[-\w]+)/$', ArtView.as_view , name="blogart"),
+    url(r'^blogpost/(?P<pk>[0-9]+)/$', HomeView.as_view , name="bloghome"),
+    url(r'^category/(?P<slug>[-\w]+)/$', CategoryView.as_view , name="blogcategory"),
+    url(r'^search/$', search , name="blogsearch"),
 ]
